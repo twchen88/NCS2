c11513d9914d5984ad68d68bc0807c9df8a59590
twchen88 <1030chloechen@gmail.com>
2021-07-14 11:56:39 -0400

    yayayayay

---
+++ evaluate.py
@@ -0,0 +1,42 @@
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+# load dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# load IR model and specify NCS
+model_path = "converted_model/saved_model.xml"
+arch_path = "converted_model/saved_model.bin"
+net = cv2.dnn.readNet(arch_path, model_path)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
+
+# use these data to test
+images = x_train.reshape(60000, 28 * 28)/255
+images = images.astype("float32")
+labels = y_train
+correct = 0
+
+# test all of them
+for i in range(100):
+    img = images[i]
+    net.setInput(img)
+    out = net.forward()
+    if np.argmax(out, axis = 1) == labels[i]:
+        correct += 1
+
+print(correct)
+# path_to_images = "gpu_test/notebooks/"
+# img = plt.imread(path_to_images + "6.jpg")
+# img = img[:, :, 0]
+# img = cv2.resize(img, (28, 28))
+# img = img.reshape(1, 28 * 28)/255
+# img = img.astype("float32")
+
+# net.setInput(img)
+# out = net.forward()
+# print(np.argmax(out, axis = 1))
